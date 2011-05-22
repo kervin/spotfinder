6b195225b73232cca4efac161e013ab6bdeeeac3
kervin <kervin152@gmail.com>
2011-05-22 15:22:26 +0400

    Added the Engine and another compare class (homefeed) The engine will contain the algorithm for calculating relationship strength
    Signed-off-by: kervin <kervin152@gmail.com>

---
+++ src/com/kervinramen/spotfinder/finder/model/Engine.java
@@ -0,0 +1,20 @@
+package com.kervinramen.spotfinder.finder.model;
+
+
+
+/**
+ * This class contains the algorithm that calculated the link
+ * strength relationship between users
+ * @author kervin
+ *
+ */
+public class Engine {
+
+    /**
+     *     
+     * @return
+     */
+    public String getName() {
+        return "";
+    }
+}
---
+++ src/com/kervinramen/spotfinder/finder/model/HomeFeedCompare.java
@@ -0,0 +1,10 @@
+package com.kervinramen.spotfinder.finder.model;
+
+/**
+ * This class calculates the number of comments in bound to a user
+ * @author kervin
+ *
+ */
+public class HomeFeedCompare {
+
+}
